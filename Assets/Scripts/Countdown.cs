ad30e6739f553c16247151e0ba8302c54c79e253
Rebecca J <rjaffe520@gmail.com>
2024-12-03 09:23:04 -0500

    complete project push

---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Countdown : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float remainingTime;
+    public GameObject timerImage;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+        }
+        else if (remainingTime < 0)
+        {
+            remainingTime = 0;
+            timerImage.SetActive(true);
+        }
+  
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+   
+
+}
